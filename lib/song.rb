39c5c49005e43c4081f81d39d3b22fa0fd01a40c
Duane Felton <dfelton2010@gmail.com>
2018-10-29 15:24:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   def self.genre_count
     count = 1
     genre_hash = {}
-    @@genres.each do |genre|
+    @@genres.each do |genre|.uniq!
       genre_hash[genre]
       genre_hash
   end
